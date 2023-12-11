4d2a2ab8cecdd3bfb5a5f77e59736c31e63a1c4d
MrMcTraher <okeaner726@gmail.com>
2023-12-11 21:00:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".subs__inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    arrows: true,
+  });
+});
